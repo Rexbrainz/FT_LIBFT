76445f8178545c671e76580c3bd516583c498b02
sudaniel <sudaniel@student.42heilbronn.de>
2024-10-23 17:33:55 +0200

    Initial commit

---
+++ ft_toupper.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 16:39:08 by sudaniel          #+#    #+#             */
+/*   Updated: 2024/10/22 14:20:23 by sudaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <ctype.h>
+#include <stdio.h>
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int main(void)
+{
+	printf("%c\t%c\n", ft_toupper('a'), ft_toupper('8'));
+	printf("%c\t%c\n", toupper('a'), toupper('8'));
+	return (0);
+}
+
